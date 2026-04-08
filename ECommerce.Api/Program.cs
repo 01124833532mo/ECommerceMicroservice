6a831e520dbd60595a323fabcca5f40357bdaa19
01124833532mo <mohammedhamdi726@gmail.com>
2026-04-08 22:21:07 +0200

    registration

--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,6 +1,18 @@
+using ECommerce.Core;
+using ECommerce.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapControllers();
 app.Run();
---
+++ ECommerce.Core/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            // Register infrastructure services here
+            // e.g., services.AddScoped<IEmailService, EmailService>();
+            return services;
+        }
+    }
+}
